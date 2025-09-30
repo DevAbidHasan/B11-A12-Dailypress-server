7aaa5ff029d23f4bbd9977c7560b80217b67f93a
DevAbidHasan <abidhasanplabon80@gmail.com>
2025-09-30 07:51:54 +0600

    added user delete feature in the admin page

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mcbondo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,34 +22,43 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-
     await client.connect();
     const db = client.db("dailyPressDB");
     const usersCollection = db.collection("users");
 
-    // POST : creating user
-    app.post("/users", async(req,res)=>{
+    // Delete : delete user by admin
+    app.delete("/user/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await usersCollection.deleteOne(query);
+        res.send(result);
+    });
+
+    // POST : creating user ---> Done
+    app.post("/users", async (req, res) => {
       const email = req.body.email;
-      const isUser = await usersCollection.findOne({email});
-      if(isUser) {
-        return res.status(200).send({message : "Already Registered, Please login.", inserted : false})
-      };
+      const isUser = await usersCollection.findOne({ email });
+      if (isUser) {
+        return res.status(200).send({
+          message: "Already Registered, Please login.",
+          inserted: false,
+        });
+      }
       const user = req.body;
       const result = await usersCollection.insertOne(user);
       res.send(result);
-    })
-    // get : see users
-    app.get("/users", async(req,res)=>{
+    });
+    // get : see users --> Done
+    app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
-    })
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
-  } 
-  finally {
+  } finally {
   }
 }
 run().catch(console.dir);
